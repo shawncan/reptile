302cd286218782f429ad4f331576fb3f69c93aa3
Shawn can <wangjiacan0216@gmail.com>
2017-03-29 07:12:46 +0800

    检查requests库性能

---
+++ mooc_bd_reptile.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/Python3
+# -*- coding: utf-8 -*-
+
+import requests
+import time
+
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text[0:1000]
+    except:
+        return "产生异常"
+
+
+def reptileTime():
+    goal = "http://www.baidu.com"
+    start = time.time()
+    for i in range(100):
+        getHTMLText(goal)
+
+    end = time.time()
+    time_consuming = time.strftime("%M:%S", time.localtime(end - start))
+    print("爬去百度100次所用的时间为:", time_consuming)
+
+
+if __name__ == "__main__":
+    reptileTime()
