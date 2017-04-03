2ea91093714d99ae58314276c5143b42b3541ac4
王佳灿 <wangjiacan0216@gmail.com>
2017-04-03 22:55:23 +0800

    2017中国大学排名

--- mooc_UniversityRankings.py
+++ mooc_UniversityRankings.py
@@ -15,11 +15,28 @@ def getHTMLText(url):
         print("爬取失败")
 
 
-if __name__ == "__main__":
-    targer = "http://python123.io/ws/demo.html"
-    demo = getHTMLText(targer)
-    soup = BeautifulSoup(demo, 'html.parser')
-    # print(soup.find_all('p'))
+def fillUnivList(ulist, html):
+    soup = BeautifulSoup(html, 'html.parser')
+    for tr in soup.find('tbody').children:
+        if tr.name is not None:
+            ts = tr('td')[0].contents
+            ulist.append([ts[0].string, ts[1].string, ts[2].string])
 
-    for tag in soup.find_all(True):
-        print(tag.name)
+
+def printUnivList(ulist, num):
+    print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "总分"))
+    for i in range(num):
+        u = ulist[i]
+        print("{:^10}\t{:^6}\t{:^10}".format(u[0], u[1], u[2]))
+
+
+def main():
+    unifo = []
+    targer = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2017.html'
+    html = getHTMLText(targer)
+    fillUnivList(unifo, html)
+    printUnivList(unifo, 50)
+
+
+if __name__ == "__main__":
+    main()
