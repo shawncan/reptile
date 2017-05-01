cb636f671370b1f428a5f8711946e6870f5f23a8
王佳灿 <wangjiacan0216@gmail.com>
2017-05-01 22:16:12 +0800

    1.优化了获取网页的函数，网页的编码直接函数输入，不再通过apparent_encoding实时的通过代码检查 2.把股票名称获取和文件写入也放到keyList判断里面了，这样程序更加清晰

--- CrawBaiduStocksB.py
+++ CrawBaiduStocksB.py
@@ -8,18 +8,18 @@ import os
 import time
 
 
-def getHTMLText(url):
+def getHTMLText(url, code="utf-8"):
     try:
         r = requests.get(url)
         r.raise_for_status()
-        r.encoding = r.apparent_encoding
+        r.encoding = code
         return r.text
     except:
         print("爬取失败")
 
 
 def getStockList(lst, stockURL):
-    html = getHTMLText(stockURL)
+    html = getHTMLText(stockURL, "GB2312")
     soup = BeautifulSoup(html, 'html.parser')
     a = soup.find_all('a')
     for i in a:
@@ -56,26 +56,28 @@ def getStockInfo(lst, stockURL, path, name):
             stockInfo = soup.find(attrs={'class': 'bets-content'})
             keyList = stockInfo.find_all('dt')
             valueList = stockInfo.find_all('dd')
+
             if keyList:
                 for i in range(len(keyList)):
                     key = keyList[i].text.split()[0]
                     val = valueList[i].text.split()[0]
                     infoDict[key] = val
+
+                name = soup.find_all(attrs={'class': 'bets-name'})[0]
+                infoDict.update({'股票名称': name.text.split()[0]})
+
+                with open(output_file, 'a', encoding='utf-8') as f:
+                    f.write(str(infoDict) + '\n')
+                    count = count + 1
+                    print("\r当前进度:{:.2f}%".format(count * 100 / len(lst), end=""))
+
+                success_count = success_count + 1
+
             else:
                 count = count + 1
                 print("\r当前进度: {:.2f}%".format(count * 100 / len(lst)), end="")
                 failure_count = failure_count + 1
                 continue
-
-            name = soup.find_all(attrs={'class': 'bets-name'})[0]
-            infoDict.update({'股票名称': name.text.split()[0]})
-
-            with open(output_file, 'a', encoding='utf-8') as f:
-                f.write(str(infoDict) + '\n')
-                count = count + 1
-                print("\r当前进度:{:.2f}%".format(count * 100 / len(lst), end=""))
-
-            success_count = success_count + 1
 
         except:
             count = count + 1
@@ -92,10 +94,8 @@ def main():
     file_path = '/Users/wangjiacan/Desktop/shawn/爬取资料/股票信息/'
     file_name = 'BaiduStockInfo.txt'
     slist = []
-    slist_1 = ['sh201005']
     start = time.time()
     getStockList(slist, stock_list_url)
-    # print(slist)
     getStockInfo(slist, stock_info_url, file_path, file_name)
     end = time.time()
     time_consuming = time.strftime("%M:%S", time.localtime(end - start))
