ab2f7e195f7d28e62c2d958b0800c4d526be4412
王佳灿 <wangjiacan0216@gmail.com>
2017-07-02 11:41:12 +0800

    修改了糗事储存方式，然后把糗事的集合列表优化了下

--- QiubaiFeatured.py
+++ QiubaiFeatured.py
@@ -3,8 +3,6 @@
 
 import requests
 from bs4 import BeautifulSoup
-import smtplib
-from email.mime.text import MIMEText
 import time
 import logging
 
@@ -13,17 +11,16 @@ class QSBK(object):
 
     def __init__(self):
         self.pageIndex = 1
-        self.stories = [['Top1'], ['Top2'], ['Top3'], ['Top4'], ['Top5'], ['Top6'], ['Top7'], ['Top8'], ['Top9'],
-                        ['Top10']]
-        self.stats = []
+        self.qs_ranking = []
         self.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0'
         self.headers = {'User-Agent': self.user_agent}
+        self.storepath = '/Users/wangjiacan/Desktop/代码/crawldata/qsbk.txt'
         self.enable = True
 
         logging.basicConfig(level=logging.WARNING,
                             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S',
-                            filename='/Users/wangjiacan/Desktop/代码/log/OtakuGirlPicture.txt',
+                            filename='/Users/wangjiacan/Desktop/代码/log/QiubaiFeatured.txt',
                             filemode='a')
 
         self.logger = logging.getLogger()
@@ -31,7 +28,7 @@ class QSBK(object):
     def getHTMLText(self):
         try:
             url = 'https://www.qiushibaike.com/hot/page/' + str(self.pageIndex)
-            r = requests.get(url, headers=self.headers)
+            r = requests.get(url, headers=self.headers, )
             r.raise_for_status()
             r.encoding = 'UTF-8'
             return r.text
@@ -54,78 +51,51 @@ class QSBK(object):
                     content_module = i.find('div', attrs={'class': 'content'})
                     funny_module = i.find('span', attrs={'class': 'stats-vote'})
 
-                    conten = content_module.find('span').text.split()[0]
+                    conten = content_module.find('span').text
                     funn = int(funny_module.find('i').text.split()[0])
 
-                    if self.stats:
-                        list_length = len(self.stats)
-                        list_small = min(self.stats)
-                        position = self.stats.index(list_small)
+                    qs_data = {'conten': conten, 'funn': funn, }
+
+                    if self.qs_ranking:
+                        list_length = len(self.qs_ranking)
+                        self.qs_ranking = sorted(self.qs_ranking, key=lambda k: k['funn'], reverse=True)
+                        list_small = self.qs_ranking[-1]['funn']
                         if list_length == 10 and funn > list_small:
-                            self.stats.insert(position, funn)
-                            self.stats.remove(list_small)
-                            self.stories[position].pop()
-                            self.stories[position].append(conten)
+                            self.qs_ranking.pop()
+                            self.qs_ranking.append(qs_data)
                         elif list_length < 10:
-                            self.stats.append(funn)
-                            self.stories[list_length].append(conten)
+                            self.qs_ranking.append(qs_data)
                         else:
                             continue
                     else:
-                        self.stats.append(funn)
-                        self.stories[0].append(conten)
+                        self.qs_ranking.append(qs_data)
                 time.sleep(2)
 
             self.pageIndex += 1
             next = soup.find('span', attrs={'class': 'next'}).text.split()[0]
             if next != "下一页":
                 self.enable = False
-            print(self.stories)
+            print(self.pageIndex)
             time.sleep(20)
         except Exception:
             self.logger.exception("Extract Page {numeral} data failed".format(numeral=self.pageIndex))
 
-
-    def mail(self):
-        html_content = ''
-        rows = 0
-        for top in self.stories:
-            for content in top:
-                rows += 1
-
-                if rows % 2 == 0:
-                    html_content = html_content + content + '\n' + '\n'
-                else:
-                    top_start = '<font size="6">'
-                    top_end = '</font>'
-                    html_content = html_content + top_start + content + top_end + '\n'
-
-        html_start = '<font size="5"， color="#000000", face="宋体"><pre>'
-        html_end = '码农：王小灿(ง⁼̴̀ω⁼̴́)ง⁼³₌₃ </pre></font>'
-
-        my_sender = '784241389@qq.com'
-        my_pass = 'vytugytpvftebdcb'
-        my_user = '784241389@qq.com'
-
-        msg = MIMEText(html_start + html_content + html_end, 'html', 'utf-8')
-        mail_time = time.strftime("%Y-%m-%d", time.localtime())
-        msg["Subject"] = mail_time + "糗事精选"
-        msg["From"] = my_sender
-        msg["To"] = my_user
-
+    def storeData(self):
         try:
-            server = smtplib.SMTP_SSL("smtp.qq.com", 465)
-            server.login(my_sender, my_pass)
-            server.sendmail(my_sender, my_user, msg.as_string())
-            server.quit()
+            with open(self.storepath, 'a') as f:
+                f.write(time.strftime("%Y-%m-%d", time.localtime()) + "\n")
+                for i in self.qs_ranking:
+                    f.write(i['conten'] + "\n")
+                f.write("\n")
         except Exception:
-            self.logger.exception("E-mail failed to send")
+            self.logger.exception("File storage failed")
+
 
     def start(self):
         while self.enable:
             self.getContentExtraction()
-
-        self.mail()
+        self.storeData()
+        print("程序运行成功！")
 
 
 spider = QSBK()
