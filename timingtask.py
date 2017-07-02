3e190b0d250825d2927de505f87f4bac6a26d1df
王佳灿 <wangjiacan0216@gmail.com>
2017-07-02 16:43:27 +0800

    定时任务脚本

---
+++ timingtask.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/Python3
+# -*- coding: utf-8 -*-
+
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+from QiubaiFeatured import QSBK
+
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=20, minute=00)
+def my_job():
+    print(
+        "{start_time}定时任务开始执行".format(start_time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))))
+    spider = QSBK()
+    spider.start()
+    print(
+        "{end_time}定时任务执行结束".format(end_time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))))
+
+sched.start()
