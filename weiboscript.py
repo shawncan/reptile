b6bb6f9055ba874fed659f3ff11349474df93422
王佳灿 <wangjiacan0216@gmail.com>
2017-07-08 22:44:49 +0800

    每天自动微博脚本完成

--- weiboscript.py
+++ weiboscript.py
@@ -5,248 +5,183 @@ import requests
 import base64
 import rsa
 import binascii
-import time
-
-# url = 'https://api.weibo.com/oauth2/authorize'
-#
-# payload = {'client_id': '2533105686', 'redirect_uri': 'https://api.weibo.com/oauth2/default.html'}
-#
-# html = requests.get(url, params=payload)
-# print(html.url)
 
 
-'''base64加密用户名'''
-username = '15557168781'
-bytesString = username.encode(encoding="utf-8")
-su = base64.b64encode(bytesString).decode('utf-8')
+class autoWeibo(object):
+    def __init__(self):
+        self.parameter_url = 'https://login.sina.com.cn/sso/prelogin.php'
+        self.ticket_url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)&_=1499149326610&openapilogin=qrcode'
+        self.code_url = 'https://api.weibo.com/oauth2/authorize'
+        self.access_token_url = 'https://api.weibo.com/oauth2/access_token'
 
-'''获取servertime、nonce、pubket、rsakv参数'''
-params = {
-    'su': su,
-    'entry': 'openapi',
-    'callback': 'sinaSSOController.preloginCallBack',
-    'rsakt': 'mod',
-    'checkpin': '1',
-    'client': 'ssologin.js(v1.4.18)',
-    '_': '1499082911503'
-}
-r = requests.get('https://login.sina.com.cn/sso/prelogin.php', params=params)
-a = r.text.split(',')
-servertime = a[1].split(':')[1]
-nonce = a[3].split(':')[1][1:-1]
-pubket = a[4].split(':')[1][1:-1]
-rsakv = a[5].split(':')[1][1:-1]
+        self.app_key = '****'
+        self.app_secret = '****'
+        self.redirect_uri = 'https://api.weibo.com/oauth2/default.html'
+        self.username = '****'
+        self.password = '****'
 
-'''获得密码rsa加密'''
-password = 'wjc0216wjc'
-rsa_e = '65537'
-key = rsa.PublicKey(int(pubket, 16), int(rsa_e))
-pw_string = str(servertime) + '\t' + str(nonce) + '\n' + str(password)
-ps = pw_string.encode(encoding="utf-8")
-pw_encypted = rsa.encrypt(ps, key)
-passwd = binascii.b2a_hex(pw_encypted)
-sp = passwd.decode()
+        self.su = ''
+        self.sp = ''
+        self.servertime = ''
+        self.nonce = ''
+        self.pubket = ''
+        self.rsakv = ''
+        self.code = ''
+        self.access_token = ''
 
-# '''获得ticket'''
-# postPara = {
-#     'appkey': '45d1VI',
-#     'cdult': '2',
-#     'ct': '1800',
-#     'domain': 'weibo.com',
-#     'door': '',
-#     'encoding': 'UTF-8',
-#     'entry': 'openapi',
-#     'from': '',
-#     'gateway': '1',
-#     'nonce': nonce,
-#     'pagerefer': '',
-#     'prelt': '1381',
-#     'pwencode': 'rsa2',
-#     'returntype': 'TEXT',
-#     'rsakv': rsakv,
-#     's': '1',
-#     'savestate': '0',
-#     'servertime': servertime,
-#     'service': 'miniblog',
-#     'sp': sp,
-#     'sr': '1440*900',
-#     'su': su,
-#     'useticket': '1',
-#     'vsnf': '1',
-#     'vsnval': '',
-# }
-#
-# ticket_url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)&_=1499149326610&openapilogin=qrcode'
-# req = requests.post(ticket_url, data=postPara)
-# ticket = req.json()['ticket']
+    def getParameter(self):
+        """
+        此函数用作获取微博登录的基础参数
+        su为用户账号、sp为用户密码
+        servertime、nonce、pubket参数为sp加密时用
+        rsakv参数为ticket时用
+        """
+        '''base64加密用户名'''
+        bytesString = self.username.encode(encoding="utf-8")
+        self.su = base64.b64encode(bytesString).decode('utf-8')
 
-#
-# '''获得code'''
-# app_key = '2811141451'
-# redirect_uri = 'https://api.weibo.com/oauth2/default.html'
-#
-# codePara = {
-#     'action': 'login',
-#     'appkey62': '6KwNnX',
-#     'client_id': app_key,
-#     'display': 'default',
-#     'from': '',
-#     'isLoginSina': '',
-#     'passwd': '',
-#     'quick_auth': 'false',
-#     'redirect_uri': redirect_uri,
-#     'regCallback': "https://api.weibo.com/2/oauth2/authorize?client_id={0}&response_type=code&display=default&redirect_uri={1}&from=&with_cookie=".format(
-#         app_key, redirect_uri),
-#     'response_type': 'code',
-#     'scope': '',
-#     'state': '',
-#     'switchLogin': '0',
-#     'ticket': ticket,
-#     'userId': '',
-#     'verifyToken': 'null',
-#     'withOfficalAccount': '',
-#     'withOfficalFlag': '0'
-# }
+        '''获取servertime、nonce、pubket、rsakv参数'''
+        params = {
+            'su': self.su,
+            'entry': 'openapi',
+            'callback': 'sinaSSOController.preloginCallBack',
+            'rsakt': 'mod',
+            'checkpin': '1',
+            'client': 'ssologin.js(v1.4.18)',
+            '_': '1499082911503'
+        }
 
-# codePara = {
-#     'action': 'authorize',
-#     'appkey62': '6KwNnX',
-#     'client_id': app_key,
-#     'display': 'default',
-#     'from': '',
-#     'isLoginSina': '',
-#     'redirect_uri': redirect_uri,
-#     'regCallback': "https://api.weibo.com/2/oauth2/authorize?client_id={0}&response_type=code&display=default&redirect_uri={1}&from=&with_cookie=".format(
-#         app_key, redirect_uri),
-#     'response_type': 'code',
-#     'scope': '',
-#     'state': '',
-#     'ticket': '',
-#     'uid': '5767076537',
-#     'url': "https://api.weibo.com/oauth2/authorize?redirect_uri={0}&client_id={1}".format(redirect_uri, app_key),
-#     'verifyToken': 'f3ea46f5ae1e913baabb6e11c836f597',
-#     'visible': '0',
-#     'withOfficalAccount': '',
-#     'withOfficalFlag': '0'
-# }
+        parameter_resp = requests.get(self.parameter_url, params=params)
+        parameter = parameter_resp.text.split(',')
+        self.servertime = parameter[1].split(':')[1]
+        self.nonce = parameter[3].split(':')[1][1:-1]
+        self.pubket = parameter[4].split(':')[1][1:-1]
+        self.rsakv = parameter[5].split(':')[1][1:-1]
 
-#
-# code_url = 'https://api.weibo.com/oauth2/authorize'
-# headers = {
-#     "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
-#     "Referer": code_url,
-#     "Content-Type": "application/x-www-form-urlencoded"}
-#
-# code_return = requests.post(code_url, headers=headers, data=codePara)
-# code = code_return.url[47:]
-#
-# '''获取access_token'''
-# app_secret = 'e8090ffc41d9e21bc195f7160abb8952'
-#
-# payload_1 = {
-#     'client_id': app_key,
-#     'client_secret': app_secret,
-#     'grant_type': 'authorization_code',
-#     'code': code,
-#     'redirect_uri': redirect_uri
-# }
-#
-# r = requests.post('https://api.weibo.com/oauth2/access_token', data=payload_1)
-# access_token = r.json()['access_token']
-# expires_in = r.json()['expires_in']
-# uid = r.json()['uid']
+        '''获得密码rsa加密'''
+        rsa_e = '65537'
+        key = rsa.PublicKey(int(self.pubket, 16), int(rsa_e))
+        pw_string = str(self.servertime) + '\t' + str(self.nonce) + '\n' + str(self.password)
+        ps = pw_string.encode(encoding="utf-8")
+        pw_encypted = rsa.encrypt(ps, key)
+        passwd = binascii.b2a_hex(pw_encypted)
+        self.sp = passwd.decode()
 
+    def getCode(self):
+        """
+        此函数用作获取微博登录应用授权的code
+        ticket为授权时必要的参数
+        code为获取access_token必要参数
+        """
+        '''获得ticket'''
+        ticket_Para = {
+            'appkey': '45d1VI',
+            'cdult': '2',
+            'ct': '1800',
+            'domain': 'weibo.com',
+            'door': '',
+            'encoding': 'UTF-8',
+            'entry': 'openapi',
+            'from': '',
+            'gateway': '1',
+            'nonce': self.nonce,
+            'pagerefer': '',
+            'prelt': '1381',
+            'pwencode': 'rsa2',
+            'returntype': 'TEXT',
+            'rsakv': self.rsakv,
+            's': '1',
+            'savestate': '0',
+            'servertime': self.servertime,
+            'service': 'miniblog',
+            'sp': self.sp,
+            'sr': '1440*900',
+            'su': self.su,
+            'useticket': '1',
+            'vsnf': '1',
+            'vsnval': '',
+        }
 
-# '''发送微博'''
-# payload_2 = {
-#     'access_token': access_token,
-#     'status': '王灿灿正在努力吃全世界的狗粮，因为已经距离上次分手52天。http://wangjiacan.com'
-# }
-#
-# r = requests.post('https://api.weibo.com/2/statuses/share.json', data=payload_2)
-#
-# print(r.json())
+        req = requests.post(self.ticket_url, data=ticket_Para)
+        ticket = req.json()['ticket']
 
+        '''获得code'''
+        code_Data = {
+            'action': 'login',
+            'appkey62': '6KwNnX',
+            'client_id': self.app_key,
+            'display': 'default',
+            'from': '',
+            'isLoginSina': '',
+            'passwd': '',
+            'quick_auth': 'false',
+            'redirect_uri': self.redirect_uri,
+            'regCallback': "https://api.weibo.com/2/oauth2/authorize?client_id={app_key}&response_type=code&display=default&redirect_uri={redirect_uri}&from=&with_cookie=".format(
+                app_key=self.app_key, redirect_uri=self.redirect_uri),
+            'response_type': 'code',
+            'scope': '',
+            'state': '',
+            'switchLogin': '0',
+            'ticket': ticket,
+            'userId': '',
+            'verifyToken': 'null',
+            'withOfficalAccount': '',
+            'withOfficalFlag': '0'
+        }
 
-# '''第二种发送微博的方法'''
-# '''获得Cookie'''
-#
-# postPara = {
-#     'encoding': 'UTF-8',
-#     'entry': 'weibo',
-#     'from': '',
-#     'gateway': '1',
-#     'nonce': nonce,
-#     'pagerefer': 'http://login.sina.com.cn/sso/logout.php?entry=miniblog&r=http://weibo.com/logout.php?backurl=%2F',
-#     'prelt': '206',
-#     'pwencode': 'rsa2',
-#     'qrcode_flag': 'false',
-#     'returntype': 'META',
-#     'rsakv': rsakv,
-#     'savestate': '7',
-#     'servertime': servertime,
-#     'service': 'miniblog',
-#     'sp': sp,
-#     'sr': '1440*900',
-#     'su': su,
-#     'url': 'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
-#     'useticket': '1',
-#     'vsnf': '1',
-# }
-#
-# ticket_url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
-# req = requests.post(ticket_url, data=postPara)
-# ticket = req.cookies
-#
-# SCF = ticket['SCF']
-# SUBP = ticket['SUBP']
-# SUHB = '0cSXZtc9OqqAlT'
-# UOR = 'www.baidu.com,weibo.com,login.sina.com.cn'
-# SINAGLOBAL = '6426740726008.886.1499221066162'
-# ULV = '1499240369901:3:3:3:1942127623044.283.1499240369894:1499233820185'
-# un = '15557168781'
-# wb_publish_fist100_5767076537 = '1'
-# YF = 'ea90f703b7694b74b62d38420b5273df'
-# SUB = ticket['SUB']
-# login = '6b98d2404605c519dc0f2c4f39e9ee5a'
-# V5 = '35ff6d315d1a536c0891f71721feb16e'
-# tentry = '-'
-# Apache = '1942127623044.283.1499240369894'
-# Page = '4c69ce1a525bc6d50f53626826cd2894'
-# ALF = ticket['ALF']
-# SSOLoginState = '1499242692'
-# wvr = '6'
-#
-# cookies = 'SCF={SCF};SUBP={SUBP};SUHB={SUHB};UOR={UOR};SINAGLOBAL={SINAGLOBAL};ULV={ULV};un={un};wb_publish_fist100_5767076537={wb_publish_fist100_5767076537};' \
-#           'YF-Ugrow-G0={YF};SUB={SUB};logi={login};YF-V5-G0={V5};_s_tentry={tentry};Apache={Apache};YF-Page-G0={Page};ALF={ALF};SSOLoginState={SSOLoginState};' \
-#           'wvr={wvr}'.format(SCF=SCF, SUBP=SUBP, SUHB=SUHB, UOR=UOR, SINAGLOBAL=SINAGLOBAL, ULV=ULV, un=un, wb_publish_fist100_5767076537=wb_publish_fist100_5767076537,
-#                              YF=YF, SUB=SUB, login=login, V5=V5, tentry=tentry, Apache=Apache, Page=Page, ALF=ALF, SSOLoginState=SSOLoginState, wvr=wvr)
-#
-#
-# payload = {
-#     '_t': '0',
-#     'appkey': '',
-#     'location': 'v6_content_home',
-#     'module': 'stissue',
-#     'pdetail': '',
-#     'pic_id': '',
-#     'pub_source': 'main_',
-#     'pub_type': 'dialog',
-#     'rank': '0',
-#     'rankid': '',
-#     'style_type': '1',
-#     'text': '王灿灿正在努力吃全世界的狗粮，因为已经距离上次分手53天',
-#     'tid': ''
-# }
-# headers = {
-#     'Referer': 'http://weibo.com/u/5767076537/home?wvr=5',
-#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0',
-#     'Content-Type': 'application/x-www-form-urlencoded',
-#     'Cookie': cookies
-# }
-#
-# a = int(time.time() * 1000)
-# url = 'http://weibo.com/aj/mblog/add?ajwvr=6&__rnd=' + str(a)
-#
-# html = requests.post(url, headers=headers, data=payload)
-# print(html.text)
+        code_headers = {
+            "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
+            "Referer": self.code_url,
+            "Content-Type": "application/x-www-form-urlencoded"}
+
+        code_return = requests.post(self.code_url, headers=code_headers, data=code_Data)
+        self.code = code_return.url[47:]
+
+    def getAccesstoken(self):
+        """
+        此函数用作获取微博接口调用时的access_token
+        access_token为调用接口时必要的参数
+        """
+        access_token_Data = {
+            'client_id': self.app_key,
+            'client_secret': self.app_secret,
+            'grant_type': 'authorization_code',
+            'code': self.code,
+            'redirect_uri': self.redirect_uri
+        }
+
+        r = requests.post(self.access_token_url, data=access_token_Data)
+        self.access_token = r.json()['access_token']
+
+    def start(self):
+        self.getParameter()
+        self.getCode()
+        self.getAccesstoken()
+
+        path = '****/count.txt'
+        with open(path, 'r') as f:
+            lines = f.readlines()
+            last_line = lines[-1]
+        days = int(last_line) + 1
+
+        weibo_Data = {
+            'access_token': self.access_token,
+            'status': '王灿灿正在努力吃全世界的狗粮，因为已经距离上次分手' + str(days) + '天。http://wangjiacan.com'
+        }
+
+        weibo = requests.post('https://api.weibo.com/2/statuses/share.json', data=weibo_Data)
+
+        try:
+            created_at = weibo.json()['created_at']
+            if not created_at.strip():
+                print(weibo.json())
+            else:
+                print(weibo.json()['created_at'])
+                with open(path, 'a') as f:
+                    f.write(str(days) + "\n")
+                    f.close()
+        except Exception:
+            print("运行失败")
+
+spider = autoWeibo()
+spider.start()
