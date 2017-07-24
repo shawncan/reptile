9d45092356858694987c5f316aef7ab06b7c461e
Shawn can <wangjiacan0216@gmail.com>
2017-07-24 17:20:22 +0800

    但个模块的信息提取

--- douban_movie.py
+++ douban_movie.py
@@ -3,6 +3,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = 'https://movie.douban.com/top250?start=0&filter='
 
@@ -13,5 +14,20 @@ soup = BeautifulSoup(html, 'html.parser')
 
 ol = soup.find('ol')
 li = ol.find_all('p', attrs={'class': ""})
-print(li[1].text)
+a = li[0].text
+print(re.findall(r'\d{4}', a)[0])
+print(re.findall(r'/\xa0.*\xa0/', a)[0][2:-2])
 
+title = ol.find_all('img', attrs={'class': ""})
+print(title[0].attrs['alt'])
+
+playable = ol.find_all('span', attrs={'class': "playable"})
+print(playable[0].text)
+
+inq = ol.find_all('span', attrs={'class': "inq"})
+print(inq[0].text)
+
+details = ol.find_all('div', attrs={'class': "pic"})
+print(details[0].em.text)
+print(details[0].a.attrs['href'])
+print(details[0].img.attrs['alt'])
