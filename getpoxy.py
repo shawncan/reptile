00489cd2c3c47a4d1e162a75318b3347ea76f90a
王佳灿 <wangjiacan0216@gmail.com>
2017-09-23 20:12:29 +0800

    米扑代理IP下载检测脚本

--- getpoxy.py
+++ getpoxy.py
@@ -10,27 +10,32 @@ def getProxy():
     """
     下载代理的ip
     """
-    api_url = 'http://proxy.mimvp.com/api/fetch.php?orderid=860170913191809460&num=200&http_type=1,2&ping_time=0.3&result_fields=1,2&result_format=json'
+    api_url = '请填写代理IP的提取链接，于IP提取网站上生成'
     result = requests.get(api_url)
     proxy_json = result.json()
     proxy = proxy_json['result']
 
     for proxy_ip in proxy:
-        url_queue.put(proxy_ip['ip:port'])
-        print("!")
+        extract_ip = proxy_ip['ip:port']
+        url_queue.put(extract_ip)
 
 
 def verification():
-    try:
-        run_time = time.strftime("%Y%m%d", time.localtime())
-        path = '/Users/wangjiacan/Desktop/shawn/爬取资料/ip/ip_' + run_time + '.txt'
+    """
+    检测代理的ip是否可用
+    """
+    success_count = 0
+    failure_count = 0
+    run_time = time.strftime("%Y%m%d", time.localtime())
+    path = '/Users/wangjiacan/Desktop/shawn/爬取资料/ip/ip_' + run_time + '.txt'
 
-        url = 'https://httpbin.org/get?show_env=1'
+    url = 'https://httpbin.org/get?show_env=1'
 
-        headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36'}
+    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36'}
 
-        for i in range(url_queue.qsize()):
-            ip = url_queue.get()
+    for i in range(url_queue.qsize()):
+        ip = url_queue.get()
+        try:
             proxies = {
                 "http": "http://" + ip,
                 "https": "http://" + ip
@@ -41,12 +46,19 @@ def verification():
             with open(path, 'a') as f:
                 f.write(ip + "\n")
                 f.close()
+            success_count += 1
             print(ip + "提取成功")
-    except Exception:
-        print("链接出错")
+        except Exception:
+            failure_count += 1
+            print(ip + "无效")
+
+    print("代理ip成功：爬取成功{}条数据，爬取失败{}条数据".format(success_count, failure_count))
 
 
-url_queue = queue.Queue()
-
-getProxy()
-print(url_queue.get())
+if __name__ == '__main__':
+    url_queue = queue.Queue()
+    print("代理ip提取开始...")
+    getProxy()
+    print("代理ip下载完成，开始检测ip可用性")
+    verification()
+    print("代理ip提取结束...")
