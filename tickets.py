6ade795e9cc6bac91c8304c924d221a43f79fe14
王佳灿 <wangjiacan0216@gmail.com>
2017-10-09 20:29:52 +0800

    解析了请求结果每个座位代表的位置

--- tickets.py
+++ tickets.py
@@ -13,8 +13,18 @@ def getHTMLText(url):
     下载目标网页源码
     """
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    proxies = {
+        "http": "http://116.236.151.166:8080",
+        "https": "http://116.236.151.166:8080"
+    }
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+    }
+
     try:
-        r = requests.get(url, verify=False)
+        r = requests.get(url, proxies=proxies, headers=headers, verify=False)
         r.raise_for_status()
         r.encoding = 'utf-8'
         return r.text
@@ -47,10 +57,29 @@ def getTrips(time, from_station, to_station):
                 '={from_station}&leftTicketDTO.to_station={to_station}&purpose_codes=ADULT'.format(time=time, from_station=from_station, to_station=to_station)
 
     html = getHTMLText(trips_url)
+    if html is None:
+        print("1")
     print(html)
     j = json.loads(str(html))
-    a = j['data']['result'][4]
-    print(a.split('|'))
+    a = j['data']['result'][6]
+    # print(a)
+    seat_information = a.split('|')
+    trips = seat_information[3]
+    advanced_soft_sleeper = seat_information[21]
+    other = seat_information[22]
+    soft_sleeper = seat_information[23]
+    soft_seat = seat_information[24]
+    no_seat = seat_information[26]
+    hard_sleeper = seat_information[28]
+    herd_seat = seat_information[29]
+    second_class = seat_information[30]
+    first_class_seat = seat_information[31]
+    normal_seat = seat_information[32]
+    lying_down = seat_information[33]
+    print("车次%s" % trips)
+    print("商务座：{normal_seat}， 一等座：{first_class_seat}， 二等座：{second_class}， 高级软卧:{advanced_soft_sleeper}， "
+          "软卧：{soft_sleeper}， 动卧：{lying_down}， 硬卧：{hard_sleeper}， 软座：{soft_seat}， 硬座：{herd_seat}， 无座："
+          "{no_seat}， 其他：{other}".format(normal_seat=normal_seat, first_class_seat=first_class_seat, second_class=second_class, advanced_soft_sleeper=advanced_soft_sleeper, soft_sleeper=soft_sleeper, lying_down=lying_down, hard_sleeper=hard_sleeper, soft_seat=soft_seat, herd_seat=herd_seat, no_seat=no_seat, other=other))
 
 
 if __name__ == '__main__':
@@ -64,5 +93,4 @@ if __name__ == '__main__':
 
     # stations_dict = getStations()
 
-    getTrips('2017-10-15', 'VRH', 'HZH')
-
+    getTrips('2017-10-10', 'HZH', 'BJP')
