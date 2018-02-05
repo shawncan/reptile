51ec4a1d67ff4bd6e6dfde81a93741c47d53cda8
王佳灿 <wangjiacan0216@gmail.com>
2018-02-05 17:01:00 +0800

    阿里特价机票网站信息爬取

--- ticketMonitoring.py
+++ ticketMonitoring.py
@@ -2,16 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from bs4 import BeautifulSoup
 import re
-import logging
-import openpyxl
-import os
 import json
-import excelOperating
-import datetime
 import time
-import configparser
 import mysqlOperating
 
 def getHTMLText(url):
@@ -46,43 +39,43 @@ def getCityCode(city):
     return code
 
 
-def getDepartureTicket():
-    city = "杭州"
-    airTicketsList = []
+def writeDatabase(airTicketsList):
+    # 写入sql语句
+    write_sql = """
+    INSERT INTO cheap_flights(departure_date, departure_city, arrival_city, price, discount, sold_out) 
+    VALUES ("{departure_date}", "{departure_city}", "{arrival_city}", "{price}", "{discount}", "{sold_out}")
+    """
+    # 查询sql语句
+    inquire_sql ="""
+    select * from cheap_flights WHERE departure_date = "{departure_date}" and departure_city = "{departure_city}"
+    """
+    success_count = 0
+    failure_count = 0
 
-    cityCode = getCityCode(city)
+    my = mysqlOperating.MYSQL()
 
-    currentTime = time.time()
-    startDate = time.strftime("%Y-%m-%d", time.localtime())
-    endDate = time.strftime("%Y-%m-%d", time.localtime(float(currentTime + 3888000)))
+    for infoData in airTicketsList:
+        departure_date = infoData['出发日期']
+        departure_city = infoData['出发城市']
+        arrival_city = infoData['到达城市']
+        price = infoData['机票价格']
+        discount = infoData['折扣']
+        sold_out = 0
 
-    departureTicketUrl = 'https://sjipiao.fliggy.com/search/cheapFlight.htm?startDate={startDate}&endDate={endDate}&' \
-                         'routes={routes}-&_ksTS=1514440974905_1979&callback=jsonp1980&ruleId=99&flag=1'.format(
-        startDate=startDate, endDate=endDate, routes=cityCode)
+        searchResult = my.queryOperation(inquire_sql.format(departure_date=departure_date, departure_city=departure_city))
 
-    airTicketsInfo = getHTMLText(departureTicketUrl)
+        if not searchResult:
+            writeState = my.executeOperation(write_sql.format(departure_date=departure_date, departure_city=departure_city,
+                                                              arrival_city=arrival_city, price=price, discount=discount, sold_out=sold_out))
+            if writeState == 0:
+                success_count += 1
+            else:
+                failure_count += 1
+        else:
+            continue
 
-    host = re.findall(r'"HOST":".*","status"', airTicketsInfo)
-    hostLength = len(host[0][8:-10])
+    print("此次数据爬取：数据库插入成功{}条数据，数据库插入失败{}条数据".format(success_count, failure_count))
 
-    lnterceptionLength = 21 + hostLength + 22
-    airTickets = airTicketsInfo.strip()[lnterceptionLength:-2]
-
-    airTicketsData = json.loads(airTickets)
-
-    flights = airTicketsData['flights']
-
-    for info in flights:
-        infoData = {'目的地':'', '机票价格':'', '出发日期':'', '折扣':''}
-
-        infoData['目的地'] = info['arrName']
-        infoData['机票价格'] = info['price']
-        infoData['出发日期'] = info['depDate']
-        infoData['折扣'] = info['discount']
-
-        airTicketsList.append(infoData)
-
-    return airTicketsList
 
 def getCheapFlights():
     # airTicketsList 特价机票信息列表
@@ -93,13 +86,11 @@ def getCheapFlights():
     target_city = "杭州"
     city_code = getCityCode(target_city)
 
-
     # 获取查询开始时间与结束时间
     # startDate 开始时间, endDate结束时间(45天后)
     currentTime = time.time()
     startDate = time.strftime("%Y-%m-%d", time.localtime())
     endDate = time.strftime("%Y-%m-%d", time.localtime(float(currentTime + 3888000)))
-
 
     for num in range(2):
         # 设置城市代码属性
@@ -110,56 +101,36 @@ def getCheapFlights():
         CheapFlightsUrl = 'https://sjipiao.fliggy.com/search/cheapFlight.htm?startDate={startDate}&endDate={endDate}&' \
                           'routes={routes}&ruleId=99'.format(startDate=startDate, endDate=endDate, routes=city_code)
 
-        print(CheapFlightsUrl)
+        airTicketsInfo = getHTMLText(CheapFlightsUrl)
 
+        # 处理url返回内容
+        # hostLength "HOST"参数的内容长度, lnterceptionLength "data"属性前字符数包含"data":
+        # airTicketsData 处理后内容字典
+        host = re.findall(r'"HOST":".*","status"', airTicketsInfo)
+        hostLength = len(host[0][8:-10])
+        lnterceptionLength = 12 + hostLength + 22
+        airTickets = airTicketsInfo.strip()[lnterceptionLength:-2]
+        airTicketsData = json.loads(airTickets)
 
+        # 提取信息
+        # depDate 出发日期, depName 出发城市, arrName 到达城市, price 机票价格, discount 折扣
+        flights = airTicketsData['flights']
+        for info in flights:
+            infoData = {'出发日期': '', '出发城市':'', '到达城市': '', '机票价格': '',  '折扣': ''}
 
+            infoData['出发日期'] = info['depDate']
+            infoData['出发城市'] = info['depName']
+            infoData['到达城市'] = info['arrName']
+            infoData['机票价格'] = info['price']
+            infoData['折扣'] = info['discount']
 
+            airTicketsList.append(infoData)
 
-
-
-
-
-
-def writeDatabase(airTicketsList):
-    # 写入sql语句
-    write_sql = """
-    INSERT INTO cheap_flights(departure_date, destination, price, discount) VALUES ("{departure_date}", "{destination}", "{price}", "{discount}")
-    """
-    # 查询sql语句
-    inquire_sql ="""
-    select * from cheap_flights WHERE departure_date = "{departure_date}" and destination = "{destination}"
-    """
-    success_count = 0
-    failure_count = 0
-
-    my = mysqlOperating.MYSQL()
-
-    for infoData in airTicketsList:
-        departure_date = infoData['出发日期']
-        destination = infoData['目的地']
-        price = infoData['机票价格']
-        discount = infoData['折扣']
-
-        searchResult = my.queryOperation(inquire_sql.format(departure_date=departure_date, destination=destination))
-
-        if not searchResult:
-            writeState = my.executeOperation(write_sql.format(departure_date=departure_date, destination=destination, price=price, discount=discount))
-            if writeState == 0:
-                success_count += 1
-            else:
-                failure_count += 1
-        else:
-            continue
-
-    print("此次数据爬取：数据库插入成功{}条数据，数据库插入失败{}条数据".format(success_count, failure_count))
+    return airTicketsList
 
 def start():
-    # airTicketsList = getDepartureTicket()
-    #
-    # writeDatabase(airTicketsList)
-
-    getCheapFlights()
+    airTicketsList = getCheapFlights()
+    writeDatabase(airTicketsList)
 
 
 if __name__ == '__main__':
