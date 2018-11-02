2ea6ff54679ed1409f83d0934313205445f4208b
王佳灿 <wangjiacan0216@gmail.com>
2018-11-02 20:20:32 +0800

    更新脚本

--- dunCode.py
+++ dunCode.py
@@ -12,20 +12,21 @@ accountInformation = [
     # {'账号': 'external.user7@163.com', '密码': 'VPFjfVOK'},
     # {'账号': 'external.user8@163.com', '密码': 'qDDaKwnJ'},
     # {'账号': 'external.user9@163.com', '密码': '4PixFJeK'},
-    {'账号': 'external.user10@163.com', '密码': 'ykbonHkY'},
-    {'账号': 'external.user11@163.com', '密码': 'dmiOJeJp'},
-    {'账号': 'external.user12@163.com', '密码': 'm5kO9xdG'},
+    # {'账号': 'external.user10@163.com', '密码': 'ykbonHkY'},
+    # {'账号': 'external.user11@163.com', '密码': 'dmiOJeJp'},
+    # {'账号': 'external.user12@163.com', '密码': 'm5kO9xdG'},
         # {'账号': 'external.user13@163.com', '密码': 'nKFg48Ww'},
         # {'账号': 'external.user14@163.com', '密码': 'SJvoFCz6'},
     {'账号': 'external.user15@163.com', '密码': 'jzIJJO4M'},
     {'账号': 'external.user16@163.com', '密码': '45vHAx2J'},
     {'账号': 'external.user17@163.com', '密码': '8oYeRAuz'},
     {'账号': 'external.user18@163.com', '密码': 'MpSSmYwz'},
-    {'账号': 'external.user19@163.com', '密码': 'rkOL8uWB'},
-    # {'账号': 'external.user20@163.com', '密码': 'uOPsFRUE'},
-    # {'账号': 'external.user21@163.com', '密码': 'lrJFZmgS'},
+        # {'账号': 'external.user19@163.com', '密码': 'rkOL8uWB'},
+        # {'账号': 'external.user20@163.com', '密码': 'uOPsFRUE'},
+        # {'账号': 'external.user21@163.com', '密码': 'lrJFZmgS'},
 
 ]
+
 
 
 numbering = time.strftime("%Y%m%d", time.localtime())
